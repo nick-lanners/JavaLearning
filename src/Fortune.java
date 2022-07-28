172a36d37e44c69cdd87f11d17785af5805896e9
Nick Lanners <nick.lanners@zonos.com>
2022-07-28 16:57:43 -0600

    first commit

---
+++ src/Fortune.java
@@ -0,0 +1,7 @@
+package src;
+
+public class Fortune {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,20 @@
+package src;
+
+import java.util.Scanner;
+
+public class Student {
+    public static void main(String[] args) {
+        double studentGPA = 3.45;
+        String studentFirstName = "Kayla";
+        String studentLastName = "Hammon";
+
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+        System.out.println("What do you want to update it to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+
+        System.out.println(studentFirstName + " " + studentLastName + " new has a GPA of " + studentGPA);
+
+    }
+}
